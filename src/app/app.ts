61cb19075a4b5afde66d9498c083dacad72408c2
EmmanuelSeujip <emmanuelseujip@gmail.com>
2026-08-17 11:48:27 +0100

    feat: implement shared layout components including header, aside, and template structures

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/shared/header/header";
+import { Aside } from "./components/shared/aside/aside";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Aside],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
