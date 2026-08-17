fe50a2075b15a5338d9927e1011464605d75f7df
EmmanuelSeujip <emmanuelseujip@gmail.com>
2026-08-17 13:43:15 +0100

    feat: implement authentication routing, add material icons, and style password checklist component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { Acceuil } from './pages/acceuil/acceuil';
+import { Connexion } from './pages/auth-user/connexion/connexion';
+import { Inscription } from './pages/auth-user/inscription/inscription';
 
 export const routes: Routes = [
   {
     path:"",
     component:Acceuil
+  },
+  {
+    path:"connexion",
+    component:Connexion
+  },
+  {
+    path:"inscription",
+    component:Inscription
   }
 ];
--- src/app/pages/auth-user/auth-user.ts
+++ src/app/pages/auth-user/auth-user.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { HeaderUser } from "../../../components/user/shared/header-user/header-user";
 
 @Component({
   selector: 'app-auth-user',
-  imports: [HeaderUser],
+  imports: [],
   templateUrl: './auth-user.html',
   styleUrl: './auth-user.css',
 })
