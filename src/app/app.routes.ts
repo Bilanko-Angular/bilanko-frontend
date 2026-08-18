06cf50c105bc1f8c5eef7a3deb29bfc6136f1c14
EmmanuelSeujip <emmanuelseujip@gmail.com>
2026-08-18 14:58:44 +0100

    Ajout de la page de connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Acceuil } from './pages/acceuil/acceuil';
 import { SalesComponent } from './pages/ventes/ventes';
 import { ChargesComponent } from './pages/charges/charges';
+import { Connexion } from './pages/auth-user/connexion/connexion';
+import { Inscription } from './pages/auth-user/inscription/inscription';
 
 export const routes: Routes = [
   {
@@ -16,5 +18,13 @@ export const routes: Routes = [
   {
     path: "charges",
     component: ChargesComponent
+  },
+  {
+    path: "connexion",
+    component:Connexion
+  },
+  {
+    path: "inscription",
+    component:Inscription
   }
 ];
