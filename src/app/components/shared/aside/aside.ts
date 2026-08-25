c1f506217baa920f11ddcdcba202e07e74697d07
EmmanuelSeujip <emmanuelseujip@gmail.com>
2026-08-25 13:45:00 +0100

    feat: implement user authentication state management and display profile information in shared header and aside components

--- src/app/components/shared/aside/aside.ts
+++ src/app/components/shared/aside/aside.ts
@@ -1,18 +1,23 @@
-// src/app/components/shared/aside/aside.ts
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { PreferencesService } from '../../../services/preferences';
+import { UserStoreService } from '../../../service/store/user/user-store.service';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-aside',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive,UpperCasePipe],
   templateUrl: './aside.html',
   styleUrls: ['./aside.css'],
 })
 export class Aside {
   private readonly router = inject(Router);
   protected readonly prefs = inject(PreferencesService);
+  protected readonly userStore = inject(UserStoreService);
+
+  readonly profileImageError = signal(false);
+
 
   logout(): void {
     this.router.navigate(['/connexion']);
